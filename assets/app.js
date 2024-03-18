30a27021b4176e8ea10973fc37a3579141633cef
Dens26 <denis.bekaert@live.fr>
2024-03-18 09:08:43 +0000

    📦 NEW: Ajout des controller Home et Contact

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,8 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import './styles/aos.css';
+import './styles/main.css';
+import './js/aos.js';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
